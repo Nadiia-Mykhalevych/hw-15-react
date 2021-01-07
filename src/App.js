b4ee95450e2b491c18f253a7b66459b26b4bb57d
Nadiia-Mykhalevych <nadiia.mykhalevych7@gmail.com>
2021-01-07 12:47:35 +0200

    add one words

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My react app</h1>
+      <h1>My first react app</h1>
         <h2>Author: Nadiia Mykhalevych</h2>
     </div>
   );
