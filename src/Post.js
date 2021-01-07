9c1dce992ff1c8f6a125d091702cfa1a079bd759
Nadiia-Mykhalevych <nadiia.mykhalevych7@gmail.com>
2021-01-07 23:01:34 +0200

    v2.0

--- src/Post.js
+++ src/Post.js
@@ -1,26 +1,25 @@
 import React from 'react';
+import './Post.css';
 const Post = (props) => {
     console.log(props);
     return(
-        <div className="">
-             <div className="Name">
-            <p>{props.author.name}</p>
-             </div>
-            <div className="Photo">
-                <img
-                    src = {props.author.photo}
-                    alt=""
-                />
-            </div>
-            <div className="Nickname">
-                <p>{props.author.nickname}</p>
+        <div className= "PostS">
+            <div className="Author">
+                 <div className="Name">
+                    {props.author.name}
+                 </div>
+                <div className="Photo">
+                    <img src = {props.author.photo} alt=""/>
+                </div>
+                <div className="Nickname">
+                    {props.author.nickname}
+                </div>
             </div>
             <div className="Content">
                 <p>{props.content}</p>
             </div>
             <div className="Image">
-                <img src = {props.image}
-                alt=""/>
+                <img src = {props.image} alt=""/>
             </div>
             <div className="Data">
                 <p>{props.date}</p>
